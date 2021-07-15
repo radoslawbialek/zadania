260cad2dd8c30e837c802f077df52bd98aff85d9
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-07-15 15:19:40 +0200

    Utworzenie data/Task

---
+++ app/src/main/java/pl/radoslawbialek/notatnik/data/Task.kt
@@ -0,0 +1,20 @@
+package pl.radoslawbialek.notatnik.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "task_table")
+@Parcelize
+data class Task(
+    val name: String,
+    val priority: Boolean = false,
+    val completion: Boolean = false,
+    val date: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+) : Parcelable {
+    val formattedDate: String
+        get() = DateFormat.getDateTimeInstance().format(date)
+}
