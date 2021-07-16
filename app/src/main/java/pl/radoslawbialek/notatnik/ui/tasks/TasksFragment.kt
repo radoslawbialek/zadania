7981800d6b387be58137fb9862060b6b34988ac9
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-07-16 16:16:09 +0200

    Dodanie onViewCreated() w TasksFragment i uzupełnienie TasksViewModel

--- app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksFragment.kt
+++ app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksFragment.kt
@@ -1,7 +1,11 @@
 package pl.radoslawbialek.notatnik.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import pl.radoslawbialek.notatnik.R
 
@@ -9,4 +13,20 @@ import pl.radoslawbialek.notatnik.R
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val taskAdapter = TasksAdapter()
+        val tasksRecyclerView = getView()?.findViewById<RecyclerView>(R.id.tasksRecyclerView)
+        tasksRecyclerView?.apply {
+            adapter = taskAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksViewModel.kt
@@ -1,6 +1,7 @@
 package pl.radoslawbialek.notatnik.ui.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import pl.radoslawbialek.notatnik.data.TaskDao
 import javax.inject.Inject
@@ -9,4 +10,5 @@ import javax.inject.Inject
 class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+    val tasks = taskDao.getTasks().asLiveData()
 }
