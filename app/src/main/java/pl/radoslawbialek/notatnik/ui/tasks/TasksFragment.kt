cc4d6b24f6518d269d45d4f0da984133028f287e
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-07-16 18:50:13 +0200

    Dodanie możliwości filtrowania zadań

--- app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksFragment.kt
+++ app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksFragment.kt
@@ -1,14 +1,18 @@
 package pl.radoslawbialek.notatnik.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import pl.radoslawbialek.notatnik.R
+import pl.radoslawbialek.notatnik.util.onQueryTextChanged
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
@@ -28,6 +32,38 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+
+        val searchItem = menu.findItem(R.id.menu_action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_action_sort_by_name -> {
+                true
+            }
+            R.id.menu_action_sort_by_date -> {
+                true
+            }
+            R.id.menu_action_hide_completed -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.menu_action_delete_completed -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksViewModel.kt
@@ -3,10 +3,18 @@ package pl.radoslawbialek.notatnik.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import pl.radoslawbialek.notatnik.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
-    val tasks = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = tasksFlow.asLiveData()
 }
