fd2dcb94f8724b435986c864cdfe4ee13a6cb243
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-07-16 17:54:18 +0200

    Usunięcie błędu niemożności utworzenia instancji TasksViewModel

--- app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksFragment.kt
+++ app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/pl/radoslawbialek/notatnik/ui/tasks/TasksViewModel.kt
@@ -1,13 +1,11 @@
 package pl.radoslawbialek.notatnik.ui.tasks
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import dagger.hilt.android.lifecycle.HiltViewModel
 import pl.radoslawbialek.notatnik.data.TaskDao
-import javax.inject.Inject
 
-@HiltViewModel
-class TasksViewModel @Inject constructor(
+class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
     val tasks = taskDao.getTasks().asLiveData()
